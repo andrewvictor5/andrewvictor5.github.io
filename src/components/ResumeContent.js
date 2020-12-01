79971020501859dcf45ec4bc7d814f4a259c7287
Andrew Victor <43546709+andrewvictor5@users.noreply.github.com>
2020-11-30 16:58:02 -0800

    Update ResumeContent.js

--- src/components/ResumeContent.js
+++ src/components/ResumeContent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Document, Page, pdfjs } from "react-pdf";
+import { Document, pdfjs } from "react-pdf";
 import Resume from '../ResumeFall2020.pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -18,7 +18,6 @@ function ResumeContent() {
           file={Resume}
           onLoadSuccess={onDocumentLoadSuccess}
         >
-          //<Page pageNumber={pageNumber} width={900} />
         </Document>
 
       </div>
